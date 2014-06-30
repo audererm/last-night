5cc59f214513b8c65f9ddb5650c1af68449862ae
audererm <audererm@gmail.com>
2014-06-30 02:39:07 -0400

    init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to last-night
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/last_night/core_test.clj
@@ -0,0 +1,7 @@
+(ns last-night.core-test
+  (:require [clojure.test :refer :all]
+            [last-night.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
